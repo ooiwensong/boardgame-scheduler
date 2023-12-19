8d9f6dd2173d745e2253c1acc1d9b2e685638a9f
ooiws <ooiwensong@gmail.com>
2023-12-19 23:16:07 +0800

    NavLink for nav bar items

--- frontend/src/routes/Root.jsx
+++ frontend/src/routes/Root.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect } from "react";
 import { HomeIcon, PlusCircleIcon, SearchIcon } from "lucide-react";
 import {
-  Link,
+  NavLink,
   Outlet,
   redirect,
   useLoaderData,
@@ -10,6 +10,7 @@ import {
 import { jwtDecode } from "jwt-decode";
 import { getMySessions } from "@/sessions";
 import { Toaster } from "@/components/ui/toaster";
+import ProfileButton from "@/components/ProfileButton";
 
 export const loader = async () => {
   try {
@@ -40,22 +41,45 @@ const Root = () => {
     <>
       <div
         id="nav-bar"
-        className="fixed left-0 right-0 top-0 z-10 bg-white px-10 py-5 text-gray-800 shadow-md"
+        className="fixed left-0 right-0 top-0 z-10 bg-white px-10 text-gray-800 shadow-md"
       >
-        <nav className="flex justify-between">
+        <nav className="mt-3 flex justify-between">
           <div id="logo">Logo</div>
-          <div id="nav-buttons" className="flex">
-            <Link to="/">
-              <HomeIcon size={30} className="mx-5 text-lg" />
-            </Link>
-            <Link to="find-session">
-              <SearchIcon size={30} className="mx-5 text-lg" />
-            </Link>
-            <Link to="/create">
-              <PlusCircleIcon size={30} className="mx-5 text-lg" />
-            </Link>
+          <div id="nav-buttons" className="mb-1 flex">
+            <NavLink
+              to="/"
+              className={({ isActive }) =>
+                isActive
+                  ? "border-b-4 border-orange-600 text-orange-600"
+                  : "text-black-600"
+              }
+            >
+              <HomeIcon size={30} className="mx-5 mb-3 text-lg" />
+            </NavLink>
+            <NavLink
+              to="find-session"
+              className={({ isActive }) =>
+                isActive
+                  ? "border-b-4 border-orange-600 text-orange-600"
+                  : "text-black-600"
+              }
+            >
+              <SearchIcon size={30} className="mx-5 mb-3 text-lg" />
+            </NavLink>
+            <NavLink
+              to="/create"
+              className={({ isActive }) =>
+                isActive
+                  ? "border-b-4 border-orange-600 text-orange-600"
+                  : "text-black-600"
+              }
+            >
+              <PlusCircleIcon size={30} className="mx-5 mb-3 text-lg" />
+            </NavLink>
           </div>
-          <div id="profile-button">Profile Button</div>
+          <div id="profile-button">
+            <ProfileButton />
+          </div>
         </nav>
       </div>
       <div id="main-content" className="absolute left-0 right-0 top-20 z-0">
